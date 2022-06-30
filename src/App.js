5c08ebfa563038102514ba1a006571a656b0c3aa
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-06-30 23:10:26 +0600

    todo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import "./App.css";
 import Navber from "./Pages/Navber/Navber";
 import Home from "./Pages/Home/Home";
 import Footer from "./Pages/Footer/Footer";
+import { Routes, Route, Link } from "react-router-dom";
+import ToDo from "./Pages/ToDo/ToDo";
 
 function App() {
   return (
     <div className="App">
       <Navber></Navber>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/toDo" element={<ToDo></ToDo>}></Route>
+      </Routes>
+
       <Footer></Footer>
     </div>
   );
--- src/Pages/Navber/Navber.js
+++ src/Pages/Navber/Navber.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navber = () => {
   return (
@@ -56,35 +57,18 @@ const Navber = () => {
               </li>
             </ul>
           </div>
-          <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <a class="btn btn-ghost normal-case text-xl">
+            <Link to="/">End-Game</Link>
+          </a>
         </div>
         <div class="navbar-center hidden lg:flex">
           <ul class="menu menu-horizontal p-0">
             <li>
-              <a>Item 1</a>
-            </li>
-            <li tabindex="0">
               <a>
-                Parent
-                <svg
-                  class="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
+                <Link to="/toDo">To-Do</Link>
               </a>
-              <ul class="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
             </li>
+
             <li>
               <a>Item 3</a>
             </li>
